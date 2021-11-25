8cfb6b0661415f9abb5896393f11f17c63da78be
iachella <iachella@student.chalmers.se>
2021-11-25 21:34:13 +0100

    add code in bookingview to import schedule, add routes for bookingview, add sample footer

---
+++ client/src/views/BookingView.vue
@@ -0,0 +1,16 @@
+<!--schedule is imported and loaded in this view-->
+<template>
+  <div>
+    <schedule/>
+  </div>
+</template>
+
+<script>
+import schedule from '../components/schedule.vue'
+export default {
+  components: { schedule },
+    setup() {
+        
+    },
+}
+</script>
