c7b0cc9eb5fa9455f76292c6737c93403b3d5df8
iachella <iachella@student.chalmers.se>
2021-11-26 10:20:45 +0100

    add navbar example, add navbar to bookingview

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<!--navbar example -->
+<template>
+  <b-navbar toggleable="lg" type="dark" variant="">
+    <router-link to="/home"><b-img :src="require('../assets/dentistimo-lens-logo.png')"></b-img></router-link>
+  </b-navbar>
+</template>
+
+<style scoped>
+button {
+    box-shadow: 0 6px 8px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
+    transition-duration: 0.4s;
+    border-radius: 8px;
+    width: 45%;
+
+}
+button:hover {
+    box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
+    background-color: gray;
+    color: white;
+}
+
+img {
+    background: url('/assets/logo.png');
+    width: 20%;
+    height: auto;
+    text-align: left;
+    margin-left:1%;
+}
+nav {
+    background: gray;
+}
+</style>
--- client/src/views/BookingView.vue
+++ client/src/views/BookingView.vue
@@ -1,14 +1,16 @@
 <!--schedule is imported and loaded in this view-->
 <template>
   <div>
+    <navbar/>
     <schedule/>
   </div>
 </template>
 
 <script>
+import navbar from '../components/navbar.vue'
 import schedule from '../components/schedule.vue'
 export default {
-  components: { schedule },
+  components: { schedule, navbar },
     setup() {
         
     },
