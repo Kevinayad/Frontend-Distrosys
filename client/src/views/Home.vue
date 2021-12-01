a9f9f447f1413b164705b4bbbdfd2a5f0b715410
iachella <iachella@student.chalmers.se>
2021-12-01 17:49:45 +0100

    [in progress] add vue-meeting-selector, add navbar to Home page

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
+    <navbar/>
     <MapboxMap/>
-
   </div>
 </template>
 
@@ -9,11 +9,15 @@
 // @ is an alias to /src
 import MapboxMap from '@/components/map.vue'
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css'
+import navbar from '../components/navbar.vue'
+// import VueMeetingSelector from 'vue-meeting-selector'
 
 export default {
   name: 'Home',
   components: {
-    MapboxMap
+    MapboxMap,
+    navbar// ,
+    // VueMeetingSelector
   }
 }
 </script>
