4c61219a329b40a642339d6fd96407841bbff245
iachella <iachella@student.chalmers.se>
2022-01-10 12:23:43 +0100

    [PAIR] booking form and map connection
    - add form implementation for booking
    - add attributes for selected clinic
    - split topic frontend in /schedule and /bookingform
    - minor button and form fixes
    
    Co-Authored-By: jihan <jihan@student.chalmers.se>
    Co-authored by: kamilay <kamilay@student.chalmers.se>

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -13,8 +13,9 @@
                           :key="index"
                           v-for="(m, index) in markers"
                           :position="m.position"              
-                          @click ="doPublish('Frontend', Clinic_ID+index++)"
+                          @click ="getSchedule(index++)"
                           @mouseover="openWindow(m, index)" 
+                          @dblclick="scrollMeTo('schedule')"
                         ></gmap-marker>
                         <gmap-info-window 
                           @closeclick="window_open=false" 
@@ -26,7 +27,6 @@
                       </gmap-info-window> 
                     </gmap-map>
                     <hr class="divider" />
-                   
                 </div>
             </div>
         </section>
@@ -99,61 +99,18 @@
                     </div>
                 </div>
                 <div class="row gx-4 gx-lg-5 justify-content-center mb-5">
-                    <div class="col-lg-6">
-                        <!-- * * * * * * * * * * * * * * *-->
-                        <!-- * * SB Forms Contact Form * *-->
-                        <!-- * * * * * * * * * * * * * * *-->
-                        <!-- This form is pre-integrated with SB Forms.-->
-                        <!-- To make this form functional, sign up at-->
-                        <!-- https://startbootstrap.com/solution/contact-forms-->
-                        <!-- to get an API token!-->
-                        <form id="contactForm" data-sb-form-api-token="API_TOKEN">
-                            <!-- Name input-->
-                            <div class="form-floating mb-3">
-                                <input class="form-control" id="name" type="text" placeholder="Enter your name..." data-sb-validations="required" />
-                                <label for="name">Full name</label>
-                                <div class="invalid-feedback" data-sb-feedback="name:required">A name is required.</div>
-                            </div>
-                            <!-- Email address input-->
-                            <div class="form-floating mb-3">
-                                <input class="form-control" id="email" type="email" placeholder="name@example.com" data-sb-validations="required,email" />
-                                <label for="email">Email address</label>
-                                <div class="invalid-feedback" data-sb-feedback="email:required">An email is required.</div>
-                                <div class="invalid-feedback" data-sb-feedback="email:email">Email is not valid.</div>
-                            </div>
-                            <!-- Phone number input-->
-                            <div class="form-floating mb-3">
-                                <input class="form-control" id="phone" type="tel" placeholder="(123) 456-7890" data-sb-validations="required" />
-                                <label for="phone">Phone number</label>
-                                <div class="invalid-feedback" data-sb-feedback="phone:required">A phone number is required.</div>
-                            </div>
-                            <!-- Message input-->
-                            <div class="form-floating mb-3">
-                                <textarea class="form-control" id="form-message" type="text" placeholder="Enter your message here..." style="height: 10rem" data-sb-validations="required"></textarea>
-                                <label for="form-message">Additional information about the reason you want to meet the dentist (optional)</label>
-                                <div class="invalid-feedback" data-sb-feedback="message:required">A message is required.</div>
-                            </div>
-                            <!-- Submit success message-->
-                            <!---->
-                            <!-- This is what your users will see when the form-->
-                            <!-- has successfully submitted-->
-                            <div class="d-none" id="submitSuccessMessage">
-                                <div class="text-center mb-3">
-                                    <div class="fw-bolder">Form submission successful!</div>
-                                    To activate this form, sign up at
-                                    <br />
-                                    <a href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
-                                </div>
-                            </div>
-                            <!-- Submit error message-->
-                            <!---->
-                            <!-- This is what your users will see when there is-->
-                            <!-- an error submitting the form-->
-                            <div class="d-none" id="submitErrorMessage"><div class="text-center text-danger mb-3">Error sending message!</div></div>
-                            <!-- Submit Button-->
-                            <div class="d-grid"><button class="btn btn-primary btn-xl disabled" id="submitButton" type="submit">Submit</button></div>
-                        </form>
+                  <div class="col-lg-6">
+                    <form id="contactForm">
+                    <b-form-input placeholder="Name" list="input-list" id="input-with-list"></b-form-input>
+                    <br>
+                    <b-form-input placeholder="Phone number" list="input-list" id="input-with-list"></b-form-input>
+                    <br>
+                    <div style="text-align:center">
+                      <button class="btn btn-primary btn-xl" :disabled="check" @click="sendForm()">{{confirmed}}</button>
+                      <br><br><br>
                     </div>
+                    </form>
+                  </div>  
                 </div>
                 <div class="row gx-4 gx-lg-5 justify-content-center">
                     <div class="col-lg-4 text-center mb-5 mb-lg-0">
@@ -237,11 +194,10 @@ export default {
         //Schedule 
          date: new Date(),
       meetingsDays: [],
-      meeting: null,
+      meeting: null,       //<-- Selected date and time
       loading: true,
       nbDaysToDisplay: 5,
-      selectedClinic: null,
-      Clinic_ID: 'Clinic',
+      selectedClinic: null,  //<-- Data type: Number
       }
   },
   computed: {
@@ -311,8 +267,7 @@ export default {
               window.alert("Appointment was successfully booked");
           } else {
             // load schedule
-            console.log(this.selectedClinic);
-            this.meetingsDays= (JSON.parse(message))[this.selectedClinic];
+            this.meetingsDays= (JSON.parse(message))['Clinic' + this.selectedClinic];
             this.loading = false;
           }
 
@@ -359,10 +314,6 @@ export default {
 },
     doPublish(topic, payload) {
       this.client.publish(topic, payload, 2, error => {
-          //TODO: check if movable
-          //replace with this.selectedClinic = 'Clinic1' to test schedule-fixes branch.
-          //and replace the message on line 30 'Clinic1' to 'GetSchedule'.
-          this.selectedClinic = payload
         if (error) {
           console.log('Publish error', error)
         }
@@ -465,12 +416,25 @@ export default {
       // hide loading
       this.loading = false;
     },
+    getSchedule(clinicNumber){
+      this.selectedClinic = clinicNumber
+      this.doPublish('Frontend/schedule', this.selectedClinic);
+    },
     //Other
     scrollMeTo(refName) {
     var element = this.$refs[refName];
     var top = element.offsetTop;
     window.scrollTo(0, top);
     },
+    sendForm(){
+      if(this.selectedClinic == null || this.meeting == null){
+        window.alert('Please select a clinic and a time to confirm your booking');
+      } else {
+        var bookingRequestData = {'dentistid': this.selectedClinic, 'date': this.meeting}
+        this.doPublish("Frontend/bookingform", JSON.stringify(bookingRequestData))
+      }
+    }
+
   },
 }
 </script>
